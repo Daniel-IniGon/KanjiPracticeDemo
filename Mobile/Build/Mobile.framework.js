21881e4b4468a1fd57e6f99da1a8246ccc034807
Daniel Iniesta Gonzalez <niraen.megar@gmail.com>
2021-10-19 17:22:26 +0200

    Mobile build - 768x1366 development build

--- Mobile/Build/Mobile.framework.js
+++ Mobile/Build/Mobile.framework.js
@@ -1305,7 +1305,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3099584;
+STATICTOP = STATIC_BASE + 3103696;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3291,7 +3291,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3099584;
+var STATIC_BUMP = 3103696;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -16009,8 +16009,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 84365;
-Module["wasmMaxTableSize"] = 84365;
+Module["wasmTableSize"] = 84429;
+Module["wasmMaxTableSize"] = 84429;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
